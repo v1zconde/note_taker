1abe8eeabe21f53ad0868b46931d11a9c5f5b336
rvasquez <rvasquez1@gmail.com>
2021-01-25 11:21:36 -0500

    fixed the *

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,13 +2,13 @@ const path = require("path");
 
 //Display index.html when access home
 module.exports = (app) => {
-  app.get("/", function (req, res) {
-    console.log("home page requested");
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
   //Display notes.html when access note
   app.get("/notes", function (req, res) {
     console.log("tables page requested");
     res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+  app.get("*", function (req, res) {
+    console.log("home page requested");
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
