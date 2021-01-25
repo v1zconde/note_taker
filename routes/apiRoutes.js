f725227257ad0737958e80baad794804f0403b93
rvasquez <rvasquez1@gmail.com>
2021-01-24 21:58:55 -0500

    deleted unnecesary files and organize js files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,12 @@
 const fs = require("fs");
 const path = require("path");
-var note;
+
 
 module.exports = (app) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) throw err;
     notes = JSON.parse(data);
   });
-  // API ROUTES
-  // ========================================================
 
   // Setup the /api/notes get route
   app.get("/api/notes", function (req, res) {
@@ -22,29 +20,18 @@ module.exports = (app) => {
     let newNote = req.body;
     notes.push(newNote);
     updateDb();
-    console.log("Added new note: " + newNote.title);
+    console.log("Add new note: " + newNote.title);
   });
-  // Retrieves a note with specific id
+  // Retrieves a note with id
   app.get("/api/notes/:id", function (req, res) {
     // display json for the notes array indices of the provided id
     res.json(notes[req.params.id]);
   });
-  // Deletes a note with specific id
+  // Deletes a note with id
   app.delete("/api/notes/:id", function (req, res) {
     notes.splice(req.params.id, 1);
     updateDb();
-    console.log("Deleted note with id " + req.params.id);
-  });
-  // VIEW ROUTES
-  // ========================================================
-  // Display notes.html when /notes is accessed
-  app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-  });
-
-  // Display index.html when all other routes are accessed
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    console.log("Del note with id " + req.params.id);
   });
   //updates the json file whenever a note is added or deleted
   function updateDb() {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,14 @@
-//const path = require("path");
+const path = require("path");
 
+//Display index.html when access home
 module.exports = (app) => {
   app.get("/", function (req, res) {
     console.log("home page requested");
-    return res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+  //Display notes.html when access note
   app.get("/notes", function (req, res) {
     console.log("tables page requested");
-    console;
-    return res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 };
